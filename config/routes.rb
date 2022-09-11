1f4ee39575fb85300423232019a7e58db0a1cd86
ayya-kh <76466532+ayya-kh@users.noreply.github.com>
2022-09-11 11:55:24 +0000

    [Finish] 3-8

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-  resources :line #ルーティングを一括して自動生成
+  resources :lists #ルーティングを一括して自動生成
 end
